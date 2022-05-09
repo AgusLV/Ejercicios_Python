ae302ea2b73f86bb08429a44e123013a5e473f44
AgusLV <104112904+AgusLV@users.noreply.github.com>
2022-05-09 14:09:14 -0300

    Update main.py

--- Ejercicio Integrador/main.py
+++ Ejercicio Integrador/main.py
@@ -1,7 +1,3 @@
-from claseCama import Cama
-
-from claseMedicamento import Medicamento
-
 from claseManejadorCamas import ManejadorCamas
 
 from claseManejadorMedicamentos import ManejadorMedicamentos
@@ -13,7 +9,7 @@ if __name__ == '__main__':
     M_camas.testCamas()
     M_medic.testMedicamentos()
 
-    print('-----ALTA DE PACIENTE-----')
+    print('\n-----ALTA DE PACIENTE-----')
     nya=input('Ingrese nombre y apellido de paciente: ')
     
     paciente=M_camas.buscaPaciente(nya)
@@ -23,22 +19,28 @@ if __name__ == '__main__':
         total_adeuda = 0.0
         for medicamento in Lista_Med:
             total_adeuda += medicamento.getPrecio()
-        print('{}{:<25}{}{:<10}{}{:<10}'.format('Paciente:',paciente.getNom(),'Cama:',paciente.getId(),'Habitacion:',paciente.getHabitacion()))
-        print('{}{:<25}{}{:<10}'.format('Diagnóstico:',paciente.getDiagnostico(),'Fecha de Internación:',paciente.getFecha_Internacion()))
-        print('{}{:<25}'.format('Fecha de Alta:',paciente.getFecha_Alta()))
-        #print('{:<25}'.format(paciente.setFecha_Alta()))
-        # paciente.cambiaEstado(False)
-        print('{:<30}{:<20}{:<20}{:<20}'.format('Medicamento/Monodroga','Presentacion','Cantidad','Precio'))
+        print('{}{:<25}{}{:<10}{}{:<10}'.format('Paciente: ',paciente.getNom(),'Cama: ',paciente.getId(),'Habitacion: ',paciente.getHabitacion()))
+        print('{}{:<22}{}{:<10}'.format('Diagnóstico: ',paciente.getDiagnostico(),'Fecha de Internación: ',paciente.getFecha_Internacion()))
+        paciente.setFecha_Alta()
+        paciente.cambiaEstado()
 
+        print('{:<35}{:<20}{:<20}{:<20}'.format('Medicamento/Monodroga','Presentacion','Cantidad','Precio'))
         for medicamento in Lista_Med:
-            print('{:<10}{}{:<20}{:<20}{:<20}'.format(medicamento.getNombre_Comercial(),'/',medicamento.getMonodroga(),medicamento.getPresentacion(),medicamento.getCantidad(),medicamento.getPrecio()))
-            print('{:<30}{:<20}{:<20}{:<20}'.format('Total Adeudado:','','',total_adeuda))
-    else: print('No se encntró al paciente')
+            print('{:<12}{}{:<22}{:<23}{:<17}{}'.format(medicamento.getNombre_Comercial(),'/',medicamento.getMonodroga(),medicamento.getPresentacion(),medicamento.getCantidad(),medicamento.getPrecio()))
+        print('{:<35}{:<20}{:<20}{:<20}'.format('Total Adeudado:','','',total_adeuda))
+    else: print('No se encontró al paciente')
 
-    print('-----PACIENTES INTERNADOS-----')
-    diagnostico=input('Ingrese un diagnostico: ')
-    Lista_Pacientes=M_camas.buscaPaciente_Diagnostico(diagnostico)
-    if(len(Lista_Pacientes)>=1):
+    print('\n-----PACIENTES INTERNADOS-----')
+    diagnostico = input('Ingrese un diagnostico: ')
+    Lista_Pacientes = M_camas.buscaPaciente_Diagnostico(diagnostico)
+    if(len(Lista_Pacientes) >= 1):
         for paciente in Lista_Pacientes:
-            print(paciente)
-    else: print(f'No se encontraron pacientes con el diagnostico {diagnostico}.')
+            print(f'\n{paciente}')
+    else: print(f'No se encontraron pacientes internados con el diagnostico {diagnostico}.\n')
+
+#/----LOTE DE PRUEBAS----/
+'''
+Alamino, Carmen
+2/5/2021
+Hepatitis C
+'''
