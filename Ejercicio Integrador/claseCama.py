cf2225f8c4fe63dc47f1a17ce307c51a5c289163
AgusLV <104112904+AgusLV@users.noreply.github.com>
2022-05-09 14:05:32 -0300

    Update claseCama.py

--- Ejercicio Integrador/claseCama.py
+++ Ejercicio Integrador/claseCama.py
@@ -1,23 +1,22 @@
 class Cama:
-    __id=0
-    __habitacion=0
-    __estado=False
-    __NomyAp=None
-    __diagnostico=''
-    __F_Intern=''
-    __F_Alta=''
-    
+    __id = 0
+    __habitacion = 0
+    __estado = False
+    __NomyAp = None
+    __diagnostico = ''
+    __F_Intern = ''
+    __F_Alta = ''
     def __init__(self,id,hab,estado,NyA,diagn,Fi,Fa):
-        self.__id=id
-        self.__habitacion=hab
-        self.__estado=estado
-        self.__NomyAp=NyA
-        self.__diagnostico=diagn
-        self.__F_Intern=Fi
-        self.__F_Alta=Fa
+        self.__id = id
+        self.__habitacion = hab
+        self.__estado = estado
+        self.__NomyAp = NyA
+        self.__diagnostico = diagn
+        self.__F_Intern = Fi
+        self.__F_Alta = Fa
 
-#    def cambiaEstado(self,band):
-#        self.__estado=band
+    def __str__(self):
+        return(f'ID: {self.__id}\nHabitacion: {self.__habitacion}\nPaciente: {self.__NomyAp}\nEstado: {self.__estado}\nDiagnostico: {self.__diagnostico}\nFecha Internacion: {self.__F_Intern}\nFecha Alta: {self.__F_Alta}')
 
     def getNom(self):
         return self.__NomyAp
@@ -37,8 +36,11 @@ class Cama:
     def getFecha_Alta(self):
         return self.__F_Alta
 
-    def __str__(self):
-        return(f'ID: {self.__id}\nHabitacion: {self.__habitacion}\nPaciente: {self.__NomyAp}\nEstado: {self.__estado}\nDiagnostico: {self.__diagnostico}\nFecha Internacion: {self.__F_Intern}\nFecha Alta: {self.__F_Alta}')
+    def setFecha_Alta(self):
+        self.__F_Alta = input('Fecha de Alta: ')
 
-#    def setFecha_Alta(self):
-#        self.__F_Alta=input('Fecha de Alta: ')
+    def getEstado(self):
+        return self.__estado
+
+    def cambiaEstado(self):
+        self.__estado = False
